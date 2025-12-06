3e72c28ed1c5e5d8b06ca27ac2cc298cbc4fe4ee
Sara King <sara@sara-king.com>
2025-12-06 00:19:53 -0600

    moved router to index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
@@ -6,14 +6,12 @@ import Resume from './pages/Resume/Resume'
 
 export default function App() {
   return (
-    <Router>
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </main>
-    </Router>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </main>
   )
 }
