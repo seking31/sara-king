014e2fa545d1cebfeabf3648daae64e9b4a9b2b6
Sara King <sara@sara-king.com>
2025-12-05 20:26:39 -0600

    updated file structure to react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,19 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
-import styled from 'styled-components'
-import logo from './logo.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-const Header = styled(Grid)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Resume from './pages/Resume/Resume'
 
-const Content = styled(Grid)`
-  display: flex;
-  justify-content: center;
-`
-
-const Icons = styled(Grid)`
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-`
-
-function App() {
+export default function App() {
   return (
-    <Grid container rowSpacing={4} marginTop={4}>
-      <Header item xs={12}>
-        <img src={logo} className="App-logo" alt="logo" />
-      </Header>
-      <Content item xs={12}>
-        Computer Science - Web Development - Cat Owner
-      </Content>
-      <Icons item xs={12}>
-        <a href="https://www.linkedin.com/in/ashleyeradford/" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="Link" icon={faLinkedin} />
-        </a>
-        <a href="https://github.com/ashleyradford" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="Link" icon={faGithub} />
-        </a>
-      </Icons>
-    </Grid>
+    <Router>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
-
-export default App
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../../index.css'
+import Navbar from '../../components/Navbar'
+
+import heroImg from '../../assets/saraNeg_ii.svg'
+import linkedinIcon from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import heroBg from '../../assets/perfect.gif'
+
+export default function Home() {
+  return (
+    <div className="site-container">
+      {' '}
+      <Navbar />
+      <main className="main-content">
+        <section className="hero" style={{ backgroundImage: `url(${heroBg})` }}>
+          <img src={heroImg} alt="Illustration of Sara King" className="hero__image" />
+        </section>
+
+        <nav className="social-links" aria-label="Social Media Links">
+          <a href="https://www.linkedin.com/in/sara-king/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedinIcon} alt="LinkedIn profile of Sara King" className="social-links__icon" />
+          </a>
+          <a href="https://github.com/seking31" target="_blank" rel="noopener noreferrer">
+            <img src={githubIcon} alt="GitHub profile of Sara King" className="social-links__icon" />
+          </a>
+        </nav>
+      </main>
+    </div>
+  )
+}
