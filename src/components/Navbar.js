427339004b5d640058daad68031921474e87ae3b
Sara King <sara@sara-king.com>
2025-12-07 16:21:18 -0600

    fixed nav bar bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-// src/components/Navbar.js
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
@@ -6,34 +5,48 @@ import './navbar.css'
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev)
+  }
+
+  const handleLinkClick = () => {
+    // Close the menu after clicking a link on mobile
+    setIsOpen(false)
+  }
+
   return (
     <header className="site-header">
-      <nav className="navbar" aria-label="Primary">
-        {/* Hamburger Button */}
-        <button className="navbar__toggle" aria-label="Toggle menu" onClick={() => setIsOpen(!isOpen)}>
-          <span className="hamburger"></span>
-          <span className="hamburger"></span>
-          <span className="hamburger"></span>
+      <nav className="navbar" aria-label="Primary navigation">
+        {/* Brand (optional, remove if not needed) */}
+        <Link to="/" className="navbar__brand">
+          MySite
+        </Link>
+
+        {/* Hamburger Button (mobile only via CSS) */}
+        <button className="navbar__toggle" aria-label="Toggle menu" aria-expanded={isOpen} onClick={handleToggle}>
+          <span className="hamburger" />
+          <span className="hamburger" />
+          <span className="hamburger" />
         </button>
 
         <ul className={`navbar__menu ${isOpen ? 'open' : ''}`}>
           <li className="navbar__item">
-            <Link to="/" className="navbar__link">
+            <Link to="/" className="navbar__link" onClick={handleLinkClick}>
               Home
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/about" className="navbar__link">
+            <Link to="/about" className="navbar__link" onClick={handleLinkClick}>
               About
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/resume" className="navbar__link">
+            <Link to="/resume" className="navbar__link" onClick={handleLinkClick}>
               Resume
             </Link>
           </li>
           <li className="navbar__item">
-            <Link to="/projects" className="navbar__link">
+            <Link to="/projects" className="navbar__link" onClick={handleLinkClick}>
               Projects
             </Link>
           </li>
